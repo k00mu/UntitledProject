84b46850892cd32edbe91b99def9433b4f5c058e
Atqa Munzir <atqamz@gmail.com>
2024-05-02 23:28:37 +0800

    add trash models and redesign game architecture

---
+++ Assets/UntitledProject/Scripts/GamePreferenceSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UntitledProject.Scripts
+{
+    [CreateAssetMenu(fileName = "NewGamePreference", menuName = "General/GamePreference")]
+    public class GamePreferenceSO : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/UntitledProject/Scripts/WorldPointer.cs
@@ -0,0 +1,55 @@
+﻿// ==================================================
+// 
+//   Created by atqamz
+// 
+// ==================================================
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UntitledProject.Scripts
+{
+	public class WorldPointer : MonoBehaviour
+	{
+		private static Camera targetCamera;
+		[SerializeField] private static LayerMask targetLayerMask;
+
+
+
+		private void Awake()
+		{
+			targetCamera = Camera.main;
+		}
+	
+	
+		public static Vector3 GetPosition()
+		{
+			Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				return Vector3.zero;
+			}
+		
+			return Physics.Raycast(ray, out RaycastHit raycastHitInfo, float.MaxValue, targetLayerMask)
+				? raycastHitInfo.point
+				: Vector3.zero;
+		}
+	
+	
+		public static Vector3 GetPosition(int _touchIndex)
+		{
+			Touch touch = Input.GetTouch(_touchIndex);
+			Ray ray = targetCamera.ScreenPointToRay(touch.position);
+		
+			if (EventSystem.current.IsPointerOverGameObject())
+			{
+				return Vector3.zero;
+			}
+
+			return Physics.Raycast(ray, out RaycastHit raycastHitInfo, float.MaxValue, MainResources.Instance.groundLayerMask)
+				? raycastHitInfo.point
+				: Vector3.zero;
+		}
+	}
+}
